8b38b8fdb6efa1c3e8906be82bea6f528c955dae
queviquev <larryk.resendiz@gmail.com>
2024-12-03 03:06:44 -0600

    blah

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import render_template
-from templates.socials import get_social
+from socials import get_social
+import os
 
 app = Flask(__name__)
 
-import os
+social = get_social()
 
 catfolder = os.path.join('static', 'cats')
 
@@ -14,30 +15,25 @@ app.config['UPLOAD_FOLDER'] = catfolder
 
 @app.route("/")
 def home():
-    social = get_social()
-    return render_template("index.html", social=social)
+    return render_template("index.html", social)
 
 @app.route("/portfolio")
 def portfolio():
-    social = get_social()
-    return render_template('portfolio.html', social=social)
+    return render_template('portfolio.html', social)
 
 @app.route("/aboutme/")
 def aboutme():
     cat1 = os.path.join(app.config['UPLOAD_FOLDER'], 'ozzyfunnyzzz.jpg')
     catList = os.listdir('static/cats')
     catList = ['cats/' + image for image in catList]
-    social = get_social()
-    return render_template('aboutme.html', social=social, catList=catList)
+    return render_template('aboutme.html', social, catList=catList)
 
 @app.route("/hireme/")
 def hireme():
-    social = get_social()
-    return render_template("hireme.html", social=social)
+    return render_template("hireme.html", social)
 
 @app.route("/privacypolicy/")
 def privacypolicy():
-    social = get_social()
     return render_template("privacypolicy.html", social=social)
 
 if __name__ == '__main__':
