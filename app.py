d442c3b1daac2c741b2ffcca20c70c10aaf7d00c
queviquev <larryk.resendiz@gmail.com>
2024-12-03 00:58:35 -0600

    asdf

--- app.py
+++ app.py
@@ -1,24 +1,43 @@
 from flask import Flask
 from flask import render_template
+from socials import get_social
 
 app = Flask(__name__)
 
+import os
+
+catfolder = os.path.join('static', 'cats')
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = catfolder
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    social = get_social()
+    return render_template("index.html", social=social)
 
-@app.route("/portfolio")
+@app.route("/portfolio/")
 def portfolio():
-    return render_template('portfolio.html')
+    social = get_social()
+    return render_template('portfolio.html', social=social)
 
 @app.route("/aboutme/")
 def aboutme():
-    return render_template('aboutme.html')
+    cat1 = os.path.join(app.config['UPLOAD_FOLDER'], 'ozzyfunnyzzz.jpg')
+    catList = os.listdir('static/cats')
+    catList = ['cats/' + image for image in catList]
+    social = get_social()
+    return render_template('aboutme.html', social=social, catList=catList)
 
 @app.route("/hireme/")
 def hireme():
-    return render_template("hireme.html")
+    social = get_social()
+    return render_template("hireme.html", social=social)
 
 @app.route("/privacypolicy/")
 def privacypolicy():
-    return render_template("privacypolicy.html")
+    social = get_social()
+    return render_template("privacypolicy.html", social=social)
+
+app.run(debug=True)
